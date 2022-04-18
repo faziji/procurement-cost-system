56cca411d386c16f67ed2b724028485b3cbbf8bd
zhuxiangfa <zhuxiangfa@huya.com>
2022-04-18 16:30:07 +0800

    chore:修改所有的权限配置

--- config/routes.ts
+++ config/routes.ts
@@ -1,3 +1,37 @@
+/**
+ * 权限管理
+ * 0. 所有权限
+ * 0.1 分析页
+ * 0.2 欢迎
+ * 0.3 个人页（个人中心、个人设置）
+ * 0.4 系统管理（征询意见、采购公告、结果公告、更正公告）
+ * 0.5 招投标管理（投标管理、成交结果）
+ * 0.6 财务管理（合同管理、发票管理）
+ * 0.7 用户管理（供应商管理、企业人事管理）
+ *
+ * 1. 采购部门
+ * 1.1 分析页
+ * 1.2 欢迎
+ * 1.3 个人页（个人中心、个人设置）
+ * 1.4 系统管理（征询意见、采购公告、结果公告、更正公告）
+ * 1.5 招投标管理（投标管理、成交结果）
+ * 1.7 用户管理（供应商管理）
+ *
+ * 2. 财务部门
+ * 2.1 分页页
+ * 2.2 欢迎页
+ * 2.3 个人页（个人中心、个人设置）
+ * 2.4 系统管理（采购公告）
+ * 2.6 财务管理（合同管理、发票管理）
+ *
+ * 3. 人事部门
+ * 3.1 分页页
+ * 3.2 欢迎页
+ * 3.3 个人页（个人中心、个人设置）
+ * 3.7 用户管理（企业人事管理）
+ *
+ * 备注：开发的时候将access全部注释，实际完成之后需要全部放开
+ */
 export default [
   {
     path: '/user',
@@ -30,24 +64,25 @@ export default [
     icon: 'smile',
     component: './Welcome',
   },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    component: './Admin',
-    routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-      {
-        component: './404',
-      },
-    ],
-  }, // 个人中心
+  // {
+  //   path: '/admin',
+  //   name: 'admin',
+  //   icon: 'crown',
+  //   access: 'canAdmin',
+  //   component: './Admin',
+  //   routes: [
+  //     {
+  //       path: '/admin/sub-page',
+  //       name: 'sub-page',
+  //       icon: 'smile',
+  //       component: './Welcome',
+  //     },
+  //     {
+  //       component: './404',
+  //     },
+  //   ],
+  // },
+  // 个人中心
   {
     path: '/account',
     name: 'account',
@@ -70,40 +105,39 @@ export default [
         component: './404',
       },
     ],
-  }, // {
-  //   name: 'list.table-list',
-  //   icon: 'table',
-  //   path: '/list',
-  //   component: './TableList',
-  // },
+  },
   {
     path: '/system',
     icon: 'SettingOutlined',
     name: '系统管理',
-    // component: './System',
+    // access: 'procurementOrFinancialRole',
     routes: [
       {
         path: '/system/consultation',
         name: '征询意见',
         icon: 'smile',
+        // access: 'procurementRole',
         component: './system/Consultation',
       },
       {
         path: '/system/purchase-announcement',
         name: '采购公告',
         icon: 'smile',
+        // access: 'procurementOrFinancialRole',
         component: './system/PurchaseAnnouncement',
       },
       {
         path: '/system/result-announcement',
         name: '结果公告',
         icon: 'smile',
+        // access: 'procurementRole',
         component: './system/ResultAnnouncement',
       },
       {
         path: '/system/correct-announcement',
         name: '更正公告',
         icon: 'smile',
+        // access: 'procurementRole',
         component: './system/CorrectAnnouncement',
       },
       {
@@ -115,6 +149,7 @@ export default [
     path: '/Tender',
     icon: 'AimOutlined',
     name: '招投标管理',
+    // access: 'procurementOrFinancialRole',
     routes: [
       {
         path: '/Tender/apply',
@@ -135,6 +170,7 @@ export default [
     path: '/Financial',
     icon: 'MoneyCollectOutlined',
     name: '财务管理',
+    // access: 'financialRole',
     routes: [
       {
         path: '/Financial/contract',
@@ -155,17 +191,20 @@ export default [
     path: '/Users',
     icon: 'UserOutlined',
     name: '用户管理',
+    // access: 'hrOrprocurementRole',
     // component: './TestPage',
     routes: [
       {
         path: '/Users/supplier',
         name: '供应商管理',
         component: './Users/Supplier',
+        // access: 'procurementRole',
       },
       {
         path: '/Users/hr',
         name: '企业人事管理',
         component: './Users/Hr',
+        // access: 'hrRole',
       },
       {
         component: './404',
--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,22 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
-export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
+export default function access(initialState: { currentUser?: any }) {
   const { currentUser } = initialState || {};
+  console.log('111111111111111111111', currentUser);
+
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    // canAdmin: currentUser && currentUser.access === 'admin',
+    canAdmin: currentUser,
+    canAdmin1: false,
+    procurementRole: currentUser && currentUser.role === 'procurement',
+    hrRole: currentUser && currentUser.role === 'hr',
+    financialRole: currentUser && currentUser.role === 'financial',
+    // 采购部门或财务部门
+    procurementOrFinancialRole:
+      currentUser && (currentUser.role === 'procurement' || currentUser.role === 'financial'),
+    // 人事部门或采购部门：用户管理（人事=>企业人事，采购=>供应商）
+    hrOrprocurementRole:
+      currentUser && (currentUser.role === 'hr' || currentUser.role === 'procurement'),
   };
 }
