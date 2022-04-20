606f74d38af5defcfd85b7fe479e4e1e08293055
faziji <zhu799353764@qq.com>
2022-04-20 22:52:03 +0800

    chore：添加部门标识

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,4 +1,4 @@
-import { Space } from 'antd';
+import { Space, Tag } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import React from 'react';
 import { useModel, SelectLang } from 'umi';
@@ -6,7 +6,6 @@ import Avatar from './AvatarDropdown';
 import HeaderSearch from '../HeaderSearch';
 import styles from './index.less';
 import NoticeIconView from '../NoticeIcon';
-
 
 export type SiderTheme = 'light' | 'dark';
 
@@ -23,42 +22,66 @@ const GlobalHeaderRight: React.FC = () => {
   if ((navTheme === 'dark' && layout === 'top') || layout === 'mix') {
     className = `${styles.right}  ${styles.dark}`;
   }
+  const role = JSON.parse(localStorage.getItem('userInfo') || '{}')?.role;
+
+  // console.log('eeeeeeeeeeeeeeeeeee', userInfo);
+
   return (
     <Space className={className}>
       <HeaderSearch
         className={`${styles.action} ${styles.search}`}
         placeholder="站内搜索"
-        defaultValue="umi ui"
+        defaultValue=""
         options={[
-          { label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>, value: 'umi ui' },
+          { label: <a href="/welcome">数据统计</a>, value: '数据统计' },
           {
-            label: <a href="next.ant.design">Ant Design</a>,
-            value: 'Ant Design',
+            label: <a href="/system/result-announcement">结果公告</a>,
+            value: '结果公告',
           },
           {
-            label: <a href="https://protable.ant.design/">Pro Table</a>,
-            value: 'Pro Table',
+            label: <a href="/system/consultation">征询意见</a>,
+            value: '征询意见',
           },
           {
-            label: <a href="https://prolayout.ant.design/">Pro Layout</a>,
-            value: 'Pro Layout',
+            label: <a href="/Financial/contract">合同管理</a>,
+            value: '合同管理',
           },
         ]}
-      // onSearch={value => {
-      //   console.log('input', value);
-      // }}
+        // onSearch={value => {
+        //   console.log('input', value);
+        // }}
       />
-      <span
+      {/* <span
         className={styles.action}
         onClick={() => {
           window.open('https://pro.ant.design/docs/getting-started');
         }}
       >
         <QuestionCircleOutlined />
-      </span>
+      </span> */}
       <NoticeIconView />
       <Avatar />
-      <SelectLang className={styles.action} />
+      {/* <Tag color="green">{role}</Tag> */}
+      {role === 'procurement' && (
+        <Tag color="green" key={role}>
+          采购部门
+        </Tag>
+      )}
+      {role === 'financial' && (
+        <Tag color="orange" key={role}>
+          财务部门
+        </Tag>
+      )}
+      {role === 'specialist' && (
+        <Tag color="blue" key={role}>
+          专家评审
+        </Tag>
+      )}
+      {role === 'hr' && (
+        <Tag color="pink" key={role}>
+          人事部门
+        </Tag>
+      )}
     </Space>
   );
 };
